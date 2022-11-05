a75357c5af185f7ebf64f3c6f111f171934ab09e
phuttonn <prestonhutton93@gmail.com>
2022-11-04 20:42:31 -0600

    Prompt user to make choice
    -store choice in playerSelection variable

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ function computerChoice() {
     let randomNumber = Math.floor(Math.random() * rpsArray.length); //takes random number and multiplies it by the length of rpsArray (3)
      return rpsArray[randomNumber]; // returns the string at that array position
 }
+
+let playerSelection = prompt("Make your Choice"); 
+
