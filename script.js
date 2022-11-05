10c1068039192332dae080b824d19e0ddd42c317
phuttonn <prestonhutton93@gmail.com>
2022-11-04 20:31:11 -0600

    Create computerChoice function
    -rename variable rockPaperScissors to rpsArray
    -Move rpsArray into computerChoice

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-let rockPaperScissors = ["Rock", "Paper", "Scissors"]; // create array to use for computerChoice
+function computerChoice() {
+    let rpsArray = ["Rock", "Paper", "Scissors"]; // create array to use for computerChoice
+    let randomNumber = Math.floor(Math.random() * rpsArray.length); //takes random number and multiplies it by the length of rpsArray (3)
+     return rpsArray[randomNumber]; // returns the string at that array position
+}
